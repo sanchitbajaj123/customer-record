cbf160ac189d8a3c8f04ad108d5c7c3950ad6405
sanchit bajaj <sanchitbajaj2003@gmail.com>
2024-05-29 01:17:19 +0530

    final

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const app = express();
 const port = 3000;
 const hostname = '0.0.0.0';
 
-
 // Middleware for serving static files
 app.use(express.static('public'));
 app.use(bodyParser.json());
@@ -22,7 +21,6 @@ mongoose.connect('mongodb+srv://sanchitbajaj2003:root@cluster0.n0euieh.mongodb.n
 // Customer schema with updated fields for date and time
 const customerSchema = new mongoose.Schema({
     name: String,
-    email: String,
     phone: String,
     frame: String,
     glasses: String,
@@ -30,18 +28,31 @@ const customerSchema = new mongoose.Schema({
     remark: String,
     prescription: {
         right: {
-            sph: String,
-            cyl: String,
-            axis: String,
-            prism: String
+            sphDV: String,
+            cylDV: String,
+            axisDV: String,
+            prismDV: String,
+            sphNV: String,
+            cylNV: String,
+            axisNV: String,
+            prismNV: String,
+            add: String
         },
         left: {
-            sph: String,
-            cyl: String,
-            axis: String,
-            prism: String
+            sphDV: String,
+            cylDV: String,
+            axisDV: String,
+            prismDV: String,
+            sphNV: String,
+            cylNV: String,
+            axisNV: String,
+            prismNV: String,
+            add: String
         }
     },
+    total: Number,
+    advance: Number,
+    balance: Number,
     dateAdded: String, // Store date when customer is added
     timeAdded: String  // Store time when customer is added
 });
@@ -59,7 +70,7 @@ app.get('/home', (req, res) => {
 app.post('/update', (req, res) => {
     const { name } = req.body;
     // Use the name to perform further operations, such as fetching the customer's details from the database
-    console.log('Received customer name:', name);
+    console.log('Received customer id:', name);
     up = name;
     console.log(up);
     res.redirect('/update');
@@ -72,9 +83,9 @@ app.get('/update', (req, res) => {
 app.post('/add-customer', async (req, res) => {
     console.log(req.body); // Log the entire request body to check if the prescription data is present
     const currentDate = new Date();
-    var date=currentDate.toLocaleDateString()
+    const date = currentDate.toLocaleDateString();
     // Extract data from request body
-    const { name, phone, frame, glasses, contactlens, remark, 'right-sph': rightSph, 'right-cyl': rightCyl, 'right-axis': rightAxis, 'right-prism': rightPrism, 'left-sph': leftSph, 'left-cyl': leftCyl, 'left-axis': leftAxis, 'left-prism': leftPrism } = req.body;
+    const { name, phone, frame, glasses, contactlens, remark, 'right-sph-dv': rightSphDV, 'right-cyl-dv': rightCylDV, 'right-axis-dv': rightAxisDV, 'right-prism-dv': rightPrismDV, 'right-sph-nv': rightSphNV, 'right-cyl-nv': rightCylNV, 'right-axis-nv': rightAxisNV, 'right-prism-nv': rightPrismNV, 'right-add': rightAdd, 'left-sph-dv': leftSphDV, 'left-cyl-dv': leftCylDV, 'left-axis-dv': leftAxisDV, 'left-prism-dv': leftPrismDV, 'left-sph-nv': leftSphNV, 'left-cyl-nv': leftCylNV, 'left-axis-nv': leftAxisNV, 'left-prism-nv': leftPrismNV, 'left-add': leftAdd, total, advance, balance } = req.body;
 
     // Create a new customer object
     const newCustomer = new Customer({
@@ -85,16 +96,36 @@ app.post('/add-customer', async (req, res) => {
         contactlens,
         remark,
         prescription: {
-            right: { sph: rightSph, cyl: rightCyl, axis: rightAxis, prism: rightPrism },
-            left: { sph: leftSph, cyl: leftCyl, axis: leftAxis, prism: leftPrism }
+            right: {
+                sphDV: rightSphDV,
+                cylDV: rightCylDV,
+                axisDV: rightAxisDV,
+                prismDV: rightPrismDV,
+                sphNV: rightSphNV,
+                cylNV: rightCylNV,
+                axisNV: rightAxisNV,
+                prismNV: rightPrismNV,
+                add: rightAdd
+            },
+            left: {
+                sphDV: leftSphDV,
+                cylDV: leftCylDV,
+                axisDV: leftAxisDV,
+                prismDV: leftPrismDV,
+                sphNV: leftSphNV,
+                cylNV: leftCylNV,
+                axisNV: leftAxisNV,
+                prismNV: leftPrismNV,
+                add: leftAdd
+            }
         },
-         dateAdded:date ,
-         timeAdded: currentDate.toLocaleTimeString(),
+        total,
+        advance,
+        balance,
+        dateAdded: date,
+        timeAdded: currentDate.toLocaleTimeString(),
     });
-    
-    // Set the current date and time
 
-    
     // Save the new customer to the database
     await newCustomer.save();
     res.status(201).send('Customer added');
@@ -107,13 +138,24 @@ app.get('/customers', async (req, res) => {
 
 app.get('/get-customer', async (req, res) => {
     try {
-        const customer = await Customer.findOne({ name: up });
+        const customer = await Customer.findOne({ _id: up });
         if (customer) {
             console.log(customer);
             res.json(customer);
         } else {
             res.status(404).send('Customer not found');
         }
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
+    }
+});
+app.get('/delete', async (req, res) => {
+    console.log("delete")
+    console.log(up)
+    try {
+            await Customer.deleteOne({ _id: up });
+            res.status(200).send('Customer deleted successfully');
+  
     } catch (error) {
         res.status(500).send('Internal Server Error');
     }
